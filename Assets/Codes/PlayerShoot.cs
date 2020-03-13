1613df3a6c3dd7087652fc69d2731c64bff6f6ec
masterrey <masterrey@gmail.com>
2020-03-13 11:07:27 -0300

    lancador de bolinha

---
+++ Assets/Codes/PlayerShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject myprojectile=
+            Instantiate(spherePrefab, transform.position+transform.forward,
+            Quaternion.identity);
+
+            myprojectile.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+
+        }
+    }
+}

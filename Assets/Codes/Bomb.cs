0a9722a6c14a9dfc4aa7c5c20522d35220d45349
masterrey <masterrey@gmail.com>
2020-03-13 12:11:41 -0300

    Bomba e tiro

---
+++ Assets/Codes/Bomb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Explode", 3);
+    }
+
+    void Explode()
+    {
+        print("Boom!");
+        Destroy(gameObject);
+        RaycastHit[] hits;
+        hits=Physics.SphereCastAll(transform.position, 5, Vector3.up, 10);
+
+        if (hits.Length > 0)
+        {
+            foreach(RaycastHit hit in hits)
+            {
+                hit.rigidbody.AddExplosionForce(1000, transform.position, 10);
+            }
+        }
+    }
+}
--- Assets/Codes/PlayerShoot.cs
+++ Assets/Codes/PlayerShoot.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public GameObject spherePrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject[] projectilesPrefab;
+    int indexWeapon;
     void Update()
     {
         //movimento de cabeca
         float movx = Input.GetAxis("Mouse Y");
         transform.Rotate(new Vector3(-movx, 0, 0));
+
+        if (Input.GetKey(KeyCode.Alpha1))indexWeapon = 0;
+
+        if (Input.GetKey(KeyCode.Alpha2)) indexWeapon = 1;
+
         //se aperta tiro instancia o prefab
         if (Input.GetButtonDown("Fire1"))
         {
             //instancia o objeto e guarda a referencia
             GameObject myprojectile=
-            Instantiate(spherePrefab, transform.position+transform.forward,
+            Instantiate(projectilesPrefab[indexWeapon], transform.position+transform.forward,
             Quaternion.identity);
             //adiciona uma forca no objeto
             myprojectile.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);

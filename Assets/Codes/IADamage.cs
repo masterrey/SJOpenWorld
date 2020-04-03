c4d5766b58849db7edc8121bb5d9ccdc5b7c41af
Reinaldo Ramos <masterrey@gmail.com>
2020-04-03 10:34:44 -0300

    IA com Malha

---
+++ Assets/Codes/IADamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IADamage : MonoBehaviour
+{
+    public int lives = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lives < 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerProjectile"))
+        {
+            lives--;
+        }
+    }
+}

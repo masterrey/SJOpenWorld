975994c40e907df73bd63b249012a75b65d238cc
Reinaldo Ramos <masterrey@gmail.com>
2020-04-17 11:51:50 -0300

    Jogador terceira pessoa e camera

---
+++ Assets/Codes/TrdCam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrdCam : MonoBehaviour
+{
+    public GameObject target;
+
+    GameObject dummy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dummy = new GameObject("DummyCam");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dummy.transform.position = target.transform.position;
+
+        transform.position = dummy.transform.position- dummy.transform.forward*3+Vector3.up*2;
+
+        transform.LookAt(target.transform.position);
+
+        dummy.transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+    }
+}
---
+++ Assets/Codes/TrdControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrdControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rdb;
+    Vector3 move, rot;
+    Animator anim;
+    public float forcemove=1000;
+    void Start()
+    {
+        rdb= GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //criacao de vetor de movimento local
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (move.magnitude > 0)
+        {
+            transform.forward = Vector3.Slerp(transform.forward,move,Time.deltaTime*10);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rdb.AddForce(move * forcemove);
+        anim.SetFloat("Velocity", rdb.velocity.magnitude);
+    }
+
+}

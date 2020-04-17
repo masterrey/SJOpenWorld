127066be70553dfe220f7652f89aa4d55787204b
Reinaldo Ramos <masterrey@gmail.com>
2020-04-17 12:48:43 -0300

    Camera e colliders

--- Assets/Codes/TrdCam.cs
+++ Assets/Codes/TrdCam.cs
@@ -7,10 +7,14 @@ public class TrdCam : MonoBehaviour
     public GameObject target;
 
     GameObject dummy;
+
+    public float ajustY=1,ajustD=3;
+
     // Start is called before the first frame update
     void Start()
     {
         dummy = new GameObject("DummyCam");
+        target.GetComponent<TrdControl>().SetDummyCam(dummy);
     }
 
     // Update is called once per frame
@@ -18,9 +22,9 @@ public class TrdCam : MonoBehaviour
     {
         dummy.transform.position = target.transform.position;
 
-        transform.position = dummy.transform.position- dummy.transform.forward*3+Vector3.up*2;
+        transform.position = dummy.transform.position- dummy.transform.forward* ajustD + Vector3.up*2;
 
-        transform.LookAt(target.transform.position);
+        transform.LookAt(target.transform.position+Vector3.up* ajustY);
 
         dummy.transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
     }
--- Assets/Codes/TrdControl.cs
+++ Assets/Codes/TrdControl.cs
@@ -9,10 +9,34 @@ public class TrdControl : MonoBehaviour
     Vector3 move, rot;
     Animator anim;
     public float forcemove=1000;
+    GameObject dummyCam;
+
+   
     void Start()
     {
         rdb= GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+
+
+        Joint[] joints;
+        joints = GetComponentsInChildren<Joint>();
+        foreach (Joint myjoint in joints)
+        {
+            Destroy(myjoint);
+        }
+
+        Rigidbody[] rdbs;
+        rdbs = GetComponentsInChildren<Rigidbody>();
+        for(int i=1;i<rdbs.Length;i++)
+        {
+            Destroy(rdbs[i]);
+        }
+        rdb.isKinematic=false;
+    }
+
+    public void SetDummyCam(GameObject dummy)
+    {
+        dummyCam = dummy;
     }
 
     // Update is called once per frame
@@ -20,6 +44,9 @@ public class TrdControl : MonoBehaviour
     {
         //criacao de vetor de movimento local
         move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
+        if (dummyCam)
+            move = dummyCam.transform.TransformDirection(move);
 
         if (move.magnitude > 0)
         {
@@ -29,8 +56,11 @@ public class TrdControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rdb.AddForce(move * forcemove);
-        anim.SetFloat("Velocity", rdb.velocity.magnitude);
+
+        float vel = rdb.velocity.magnitude;
+
+        rdb.AddForce((move * forcemove)/ (vel*2+1));
+        anim.SetFloat("Velocity", vel);
     }
 
 }
